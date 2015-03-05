2b40c16ff31bf64c9f2b79f47bbd715e6563275d
Kellen Lask <kellenlask@gmail.com>
2015-03-04 20:13:13 -0700

    STARTED CODING!
    -Created Meeting class
    -Renamed important views in the MeetingParams Activity
    -Started in on the MeetingParams Activity Java Code
    -Created a method to initialize the View fields
    -Created a method to get the Days CheckBoxes' states
    -Created private fields for the important Views - duh

---
+++ app/src/main/java/Meeting.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Kellen on 3/4/2015.
+ */
+public class Meeting {
+//Fields
+	private boolean[] days;
+
+//Constructor
+	public Meeting(boolean[] days) {
+		this.days = days; //Length 7 (1 for each day) true or false for available or not.
+
+	} //End constructor
+
+//Accessors
+
+
+//Mutators
+
+
+
+} //End public class Meeting
--- app/src/main/java/com/bk/fm/findmeeting/MeetingParams.java
+++ app/src/main/java/com/bk/fm/findmeeting/MeetingParams.java
@@ -2,13 +2,85 @@ package com.bk.fm.findmeeting;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 
 public class MeetingParams extends ActionBarActivity {
+//----------------------------------------------------
+//
+//	Fields
+//
+//----------------------------------------------------
+	private CheckBox sunday;
+	private CheckBox monday;
+	private CheckBox tuesday;
+	private CheckBox wednesday;
+	private CheckBox thursday;
+	private CheckBox friday;
+	private CheckBox saturday;
 
+	private Button nextButton;
+
+	private TextView startTime;
+	private TextView endTime;
+	private TextView meetingDuration;
+
+
+//----------------------------------------------------
+//
+//	"Constructor"
+//
+//----------------------------------------------------
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_meeting_params);
-	}
-}
+
+		initializeFields();
+	} //End protected void onCreate(Bundle)
+
+//----------------------------------------------------
+//
+//	Logical Methods
+//
+//----------------------------------------------------
+
+	//Make a boolean array representing the available days as selected by the user via checkboxes.
+	public boolean[] makeDaysArray(){
+		boolean[] days = new boolean[7];
+
+		days[0] = sunday.isChecked();
+		days[1] = monday.isChecked();
+		days[2] = tuesday.isChecked();
+		days[3] = wednesday.isChecked();
+		days[4] = thursday.isChecked();
+		days[5] = friday.isChecked();
+		days[6] = saturday.isChecked();
+
+		return days;
+	} //End public boolean[] makeDaysArray()
+
+	public void initializeFields() {
+	//Checkboxes
+		sunday = (CheckBox) findViewById(R.id.sunday);
+		monday = (CheckBox) findViewById(R.id.monday);
+		tuesday = (CheckBox) findViewById(R.id.tuesday);
+		wednesday = (CheckBox) findViewById(R.id.wednesday);
+		thursday = (CheckBox) findViewById(R.id.thursday);
+		friday = (CheckBox) findViewById(R.id.friday);
+		saturday = (CheckBox) findViewById(R.id.saturday);
+
+	//Button
+		nextButton = (Button) findViewById(R.id.nextButton);
+
+	//Time Inputs
+		startTime = (TextView) findViewById(R.id.startTime);
+		endTime = (TextView) findViewById(R.id.endTime);
+		meetingDuration = (TextView) findViewById(R.id.duration);
+
+	} //End public void initializeFields()
+
+
+} //End class MeetingParams extends ActionBarActivity
